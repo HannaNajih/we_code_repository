db5679cb21b449a51f533b9cb60544e094662126
Hanna <hnajih325@gmail.com>
2022-06-19 16:18:24 +0300

    seconf commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,17 +8,52 @@ void main() {
 }
 
 class helo_kurdistan extends StatelessWidget {
-  const helo_kurdistan({Key? key}) : super(key: key);
-
+  static const primaryColor = Color.fromARGB(255, 15, 87, 27);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("hello kurdistan"),
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Kurdish Flag"),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150.0,
+              width: 280.0,
+              color: Colors.transparent,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: new Center(
+                    child: new Image.asset(
+                      'images/Kurdistan_Flag.jpg',
+                      height: 350,
+                      fit: BoxFit.cover,
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+                child: Text(" Hello Kurdistan",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.purpleAccent)))
+          ],
         ),
       ),
-    );
+    ));
   }
 }
